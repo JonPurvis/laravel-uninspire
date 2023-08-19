5bbd81522b1f3b42269645f8b37aaba0232de710
JonPurvis <jonathan.purvis@visualsoft.co.uk>
2023-08-19 02:58:30 +0100

    more work

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-Coming Soon
+# 😕 Introduction
+
+If you're familiar with Laravel, then you should be familiar with _artisan inspire_. 
+It's a simple command that outputs an inspirational quote when you run it.
+
+I thought it would be fun to build the opposite of that command. A command
+that when ran, spits out a humourous uninspiring quote. 
+
+## Usage
+
+```bash
+php artisan uninspire
+```
+
+## Contributing
+Contributions are always welcome! If you have an uninspiring quote that you wish
+to share, feel free to submit a Pull Request!
+
--- src/Providers/UninspireServiceProvider.php
+++ src/Providers/UninspireServiceProvider.php
@@ -1,15 +1,22 @@
 <?php
 
+declare(strict_types=1);
+
+namespace JonPurvis\Uninspire\Providers;
+
 use Illuminate\Support\ServiceProvider;
+use JonPurvis\Uninspire\Console\Commands\UninspireCommand;
 
 class UninspireServiceProvider extends ServiceProvider
 {
     public function boot(): void
     {
-        $this->commands(
-            commands: [
-                UninspireCommand::class
-            ]
-        );
+        if ($this->app->runningInConsole()) {
+            $this->commands(
+                commands: [
+                    UninspireCommand::class
+                ]
+            );
+        }
     }
 }
